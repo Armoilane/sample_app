84c300ed0785547ce4534070f075521b7e9e4a2a
Ari Moilanen <armo@iki.fi>
2021-05-27 18:49:01 +0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalifi",
+                                         password: "foo",
+                                         password_confirmation: "bar" }
+                                }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Eero Esim",
+                                         email: "ee@ro.rsi",
+                                         password: "salasana",
+                                         password_confirmation: "salasana" }
+                                }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
+  end
+  
+end
